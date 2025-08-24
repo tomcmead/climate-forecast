9bac2c8ad8576379f2eb86bd016695fa20d96346
Tom <tomcmead0@gmail.com>
2025-08-24 12:38:08 +1000

    Add remove whitespace for input city name for URL API

--- climate_analysis/lib/climate_data/src/climate_data.cpp
+++ climate_analysis/lib/climate_data/src/climate_data.cpp
@@ -1,4 +1,6 @@
 #include "climate_data.hpp"
+#include <algorithm> // Required for std::remove_if
+#include <cctype>    // Required for std::isspace
 
 // @brief Constructor for ClimateData class.
 // @param http Unique pointer to an IHttpHandler instance for HTTP requests.
@@ -13,7 +15,8 @@ ClimateData::ClimateData(std::unique_ptr<IHttpHandler> http) : http_handle(std::
 // @param city Name of city to geocode.
 // @return pair containing the (latitude, longitude) of city.
 std::optional<std::pair<double, double>> ClimateData::get_city_coordinates(const std::string& city) {
-    const std::string url = climate_api::geocode_url + city;
+    std::string url = climate_api::geocode_url + city;
+    url.erase(std::remove_if(url.begin(), url.end(), ::isspace), url.end());
 
     auto response = http_handle->http_get(url);
     if (!response.has_value()) {
